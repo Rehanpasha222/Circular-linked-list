6e765bef92856cced8e87fbe6c703184e322a107
Rehanpasha222 <rehanpasha2570@gmail.com>
2025-07-27 19:35:22 +0530

    Overflow condition

--- program.c
+++ program.c
@@ -6,3 +6,6 @@ int queue[SIZE];
 int front = -1, rear = -1;
 void enqueue(int value) {
 	 if ((front == 0 && rear == SIZE - 1) || (rear + 1) % SIZE == front) {
+		printf("Queue is full (Overflow)\n");
+        return;
+    }
